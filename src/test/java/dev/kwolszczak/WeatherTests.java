768c2ff882afe604efc58322cd440b828d1342e4
Kamil Wolszczak <kwolszczak@sii.pl>
2024-01-04 10:14:47 +0100

    data provider with test.properties

--- src/test/java/dev/kwolszczak/WeatherTests.java
+++ src/test/java/dev/kwolszczak/WeatherTests.java
@@ -1,5 +1,6 @@
 package dev.kwolszczak;
 
+import dev.kwolszczak.util.DataProvider;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.http.ContentType;
 import io.restassured.path.json.JsonPath;
@@ -10,55 +11,43 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.io.IOException;
+import java.util.Properties;
 import java.util.stream.Stream;
 
 import static io.restassured.RestAssured.given;
 
 class WeatherTests {
 
-    private String apiKey;
     private RequestSpecification req;
 
     @BeforeEach
-    void setUp() {
-        apiKey = "89a2ed8a594cc497a6273490e7ca59dd";
+    void setUp() throws IOException {
+        Properties properties = DataProvider.getProperties("test.properties");
+        String apiKey = properties.getProperty("apiKey");
+        String url = properties.getProperty("baseUri");
 
         req = new RequestSpecBuilder()
-                .setBaseUri("http://api.openweathermap.org")
+                .setBaseUri(url)
                 .addQueryParam("appid", apiKey)
                 .setContentType(ContentType.JSON)
                 .build();
     }
 
     @ParameterizedTest
-    @MethodSource("dataProvider")
+    @MethodSource("dev.kwolszczak.util.DataProvider#dataProvider")
     void shouldGetWeatherForACity(String city) {
 
-        JsonPath location = given()
-                        .spec(req)
-                        .basePath("/geo/1.0")
-                        .queryParam("q", city)
-                        .queryParam("limit", 2)
-                .when().get("/direct")
-                .then().assertThat().statusCode(200)
-                .extract().jsonPath();
-
-        var lat = location.getFloat("[0].lat");
-        var lon = location.getFloat("[0].lon");
-
+        Location location = new Location(city);
         var weather =given()
                 .spec(req)
                 .basePath("/data/2.5/weather")
-                .queryParam("lat", lat)
-                .queryParam("lon", lon)
+                .queryParam("lat", location.getLat())
+                .queryParam("lon", location.getLon())
         .when().get()
         .then().assertThat().statusCode(200)
                 .extract().jsonPath();
 
         System.out.println(weather.getString("weather[0].description"));
-    }
-
-    public static Stream<Arguments> dataProvider() {
-        return Stream.of(Arguments.of("London"), Arguments.of("Oxford"), Arguments.of("Gdańsk"));
     }
 }
---
+++ src/test/java/dev/kwolszczak/util/DataProvider.java
@@ -0,0 +1,30 @@
+package dev.kwolszczak.util;
+
+import org.junit.jupiter.params.provider.Arguments;
+
+import java.io.*;
+import java.util.Properties;
+import java.util.stream.Stream;
+
+public class DataProvider {
+
+    public static Properties getProperties(String path) throws IOException {
+
+        var properties = new Properties();
+        ClassLoader classLoader = ClassLoader.getSystemClassLoader();
+        try (InputStream resourceAsStream = classLoader.getResourceAsStream(path)) {
+            if (resourceAsStream == null) {
+                throw new FileNotFoundException(" File not found: " + path);
+            }
+
+            try (var reader = new BufferedReader(new InputStreamReader(resourceAsStream))) {
+                properties.load(reader);
+                return properties;
+            }
+        }
+    }
+
+    public static Stream<Arguments> dataProvider() {
+        return Stream.of(Arguments.of("London"), Arguments.of("Oxford"), Arguments.of("Gdańsk"));
+    }
+}
