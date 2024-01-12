4a65444e81e6cbe633424c7427074f63648ac661
Kamil Wolszczak <kwolszczak@sii.pl>
2024-01-12 14:06:50 +0100

    studentTest

--- src/test/java/dev/kwolszczak/student/StudentTest.java
+++ src/test/java/dev/kwolszczak/student/StudentTest.java
@@ -2,9 +2,9 @@ package dev.kwolszczak.student;
 
 import dev.kwolszczak.student.model.Student;
 import dev.kwolszczak.student.steps.Step;
-import dev.kwolszczak.student.BaseTest;
 import org.junit.jupiter.api.Test;
 
+import static dev.kwolszczak.student.steps.Step.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class StudentTest extends BaseTest {
@@ -16,21 +16,21 @@ class StudentTest extends BaseTest {
         String newMiddleName = properties.getProperty("newMiddleName");
 
         //POST -create student
-        int studentId = Step.createStudent(testStudent);
-        var createdStudent = Step.getStudent(studentId);
+        int studentId = createStudent(testStudent);
+        var createdStudent = getStudent(studentId);
 
         assertThat(createdStudent).usingRecursiveComparison().ignoringFields("id").isEqualTo(testStudent);
 
         //PUT-update middle name
         testStudent.setMiddle_name(newMiddleName);
-        Step.updateStudent(testStudent);
-        var updatedStudent = Step.getStudent(studentId);
+        updateStudent(testStudent);
+        var updatedStudent = getStudent(studentId);
 
         assertThat(updatedStudent.getMiddle_name()).isEqualTo(newMiddleName);
 
         //delete
         Step.deleteStudent(studentId);
-        var deletedStudent = Step.getStudent(studentId);
+        var deletedStudent = getStudent(studentId);
 
         assertThat(deletedStudent).isNull();
     }
